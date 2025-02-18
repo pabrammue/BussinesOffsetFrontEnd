98f54e889e0f15c7d436045410a17744ef56892c
pCarbonero <pablo.carbonero@iesnervion.es>
2025-02-18 14:40:43 +0100

    [upd] ayuda

--- ProyectoFrontEnd/ProyectoAngular/src/app/app.component.ts
+++ ProyectoFrontEnd/ProyectoAngular/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, MatSidenavModule, MatListModule, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
