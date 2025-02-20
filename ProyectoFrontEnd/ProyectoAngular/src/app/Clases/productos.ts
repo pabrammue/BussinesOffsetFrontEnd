e70c516f75d05ced6a07d790d7b9b8a6c2d1d87d
Danielip1705 <daniel.iglesias@iesnervion.es>
2025-02-20 14:21:26 +0100

    (new) Interfaz Producto

---
+++ ProyectoFrontEnd/ProyectoAngular/src/app/Clases/productos.ts
@@ -0,0 +1,9 @@
+export interface Productos {
+    id:number
+    nombre:string
+    precioUnitario:number
+    baseImponible:number
+    stock:number
+    idProveedor:number
+    idCategoria:number
+}
