8588c2162b9492b13142611ebb39e1023ec9a614
rubenruizc <ruben.ruiz@iesnervion.es>
2025-03-06 13:13:02 +0100

    Cambios

--- ProyectoFrontEnd/ProyectoAngular/src/app/components/layout/layout.component.ts
+++ ProyectoFrontEnd/ProyectoAngular/src/app/components/layout/layout.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon'; // Ya lo importaste aquí
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, MatButtonModule, MatSidenavModule, MatListModule, MatToolbarModule, RouterLink, RouterLinkActive],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    RouterLink,
+    RouterLinkActive,
+    MatIconModule // Añade MatIconModule aquí
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
-
 }
